99eddc41896bbf2967ea0ab202b0f1500072494e
Andrew Toups <mistertoups@gmail.com>
2016-11-14 14:52:34 -0500

    Changed directory structure

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -16,43 +16,43 @@
             controller: 'gameCtrl as game'
           }).state('main.game.play', {
             url: 'play',
-            templateUrl: './src/views/play.html',
+            templateUrl: './src/views/game/play.html',
             controller: 'playCtrl as play'
           }).state('main.game.editor', {
             // Includes sidebar and nav for all editor views
             // "/game/editor"
             url: 'editor',
-            templateUrl: './src/views/editor.html',
+            templateUrl: './src/views/game/editor.html',
             controller: 'editorCtrl as editor'
           }).state('main.game.editor.map', {
             // "/game/editor/map"
             url: 'map',
-            templateUrl: './src/views/editor/map.html',
+            templateUrl: './src/views/game/editor/map.html',
             controller: 'mapCtrl as map'
           }).state('main.game.editor.scene', {
             // "/game/editor/scene"
             url: 'scene',
-            templateUrl: './src/views/editor/scene.html',
+            templateUrl: './src/views/game/editor/scene.html',
             controller: 'sceneCtrl as scene'
           }).state('main.game.editor.bg', {
             // "/game/editor/bg"
             url: 'bg',
-            templateUrl: './src/views/editor/bg.html',
+            templateUrl: './src/views/game/editor/bg.html',
             controller: 'bgCtrl as bg'
           }).state('main.game.editor.obj', {
             // "/game/editor/obj"
             url: 'obj',
-            templateUrl: './src/views/editor/obj.html',
+            templateUrl: './src/views/game/editor/obj.html',
             controller: 'objCtrl as obj'
           }).state('main.game.editor.ent', {
             // "/game/editor/ent"
             url: 'ent',
-            templateUrl: './src/views/editor/ent.html',
+            templateUrl: './src/views/game/editor/ent.html',
             controller: 'entCtrl as ent'
           }).state('main.game.editor.scripts', {
             // "/game/editor/scripts"
             url: 'scripts',
-            templateUrl: './src/views/editor/scripts.html',
+            templateUrl: './src/views/game/editor/scripts.html',
             controller: 'scriptsCtrl as scripts'
           }).state('main.profile', {
             url: 'profile',
