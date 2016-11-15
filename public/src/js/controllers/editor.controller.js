fb9599e53d9bd7c405e12c621cbc4fd17bcaab1b
Andrew Toups <mistertoups@gmail.com>
2016-11-15 15:37:43 -0500

    Added experimental jquery stuff

--- public/src/js/controllers/editor.controller.js
+++ public/src/js/controllers/editor.controller.js
@@ -1,3 +1,9 @@
 angular.module('questCreator').controller('editorCtrl', function($state) {
   this.backgroundName = "Testing Background";
+  $('.picker').draggable();
+  $('#bg-canvas').droppable({
+    drop: function(event, ui){
+      console.log("drop it like it's hot");
+    }
+  });
 });
