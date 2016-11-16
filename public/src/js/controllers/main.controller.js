fdd5d6e4f3b6d548d3039fb303aa11e2e39c687d
Jason Carr <jasonhcarr@gmail.com>
2016-11-16 10:44:04 -0500

    made sigin/signout functions; added register cancel and goHome functions

--- public/src/js/controllers/main.controller.js
+++ public/src/js/controllers/main.controller.js
@@ -1,21 +1,31 @@
 angular.module('questCreator').controller('mainCtrl', function(socket, $state, UserService) {
 
+    //When the user clicks "Home" on the nav bar view is changed to landing
+    this.goHome = function () {
+        $state.go('main.landing');
+    };
+
+    //When the user clicks "Profile" on the nav bar user information is loaded and view is changed to profile
     this.goToUser = function () {
         var games = UserService.games();
         games.done(function () {
           $state.go('main.profile');
         });
-
     };
 
     // When the user clicks the sign in button, prompt them to sign in to their google account.
-    $('#login').click(function() {
+    this.signIn = function() {
         UserService.signIn();
-    });
+    };
 
     // When the user clicks the sign out button, sign them out of their google account
-    $('#logout').click(function() {
+    this.signOut = function() {
         UserService.signOut();
-    });
+    };
 
+    //If the user chooses not to register, they can cancel out of the process.
+    this.cancel = function () {
+        $('#register').css('display', 'none');
+        UserService.signOut();
+    };
 });
