e53db566ed7a1c262faae3395fb5874201406f2f
Jason Carr <jasonhcarr@gmail.com>
2016-11-16 14:42:57 -0500

    starting on profile controller

--- public/src/js/controllers/profile.controller.js
+++ public/src/js/controllers/profile.controller.js
@@ -1,3 +1,9 @@
-angular.module('questCreator').controller('profileCtrl', function(socket, $state, $scope) {
-  
+angular.module('questCreator').controller('profileCtrl', function(socket, $state, $scope, UserService) {
+
+  this.user = UserService.get();
+
+  this.getJoinedDate = function(date) {
+    return new Date(date);
+};
+
 });
