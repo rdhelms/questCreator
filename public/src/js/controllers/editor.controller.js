92c1c3d97d40908dbfba84ddd165b5ebb7549df1
Andrew Toups <mistertoups@gmail.com>
2016-11-15 22:41:48 -0500

    Rigged up dummy assets for ui drag and drop

--- public/src/js/controllers/editor.controller.js
+++ public/src/js/controllers/editor.controller.js
@@ -1,9 +1,20 @@
 angular.module('questCreator').controller('editorCtrl', function($state) {
   this.backgroundName = "Testing Background";
-  $('.picker').draggable();
+  $('.asset').draggable({
+    helper: 'clone',
+    start: function(event, ui) {
+      $(ui.helper).addClass('grabbed');
+    },
+    stop: function(event, ui) {
+      $(ui.helper).css({'transition': 'transform ease 100ms'}).removeClass('grabbed');
+    }
+  });
   $('#bg-canvas').droppable({
-    drop: function(event, ui){
-      console.log("drop it like it's hot");
+    drop: function(event, ui) {
+      console.log('ui', ui);
+      var clone = $(ui.draggable).clone();
+      clone.draggable();
+      $(this).append(clone);
     }
   });
 });
