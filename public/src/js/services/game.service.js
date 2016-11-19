325729999d57519d0239aaaaeb24e2f26c440b9e
Jason Carr <jasonhcarr@gmail.com>
2016-11-19 17:48:46 -0500

    removed some testing data

--- public/src/js/services/game.service.js
+++ public/src/js/services/game.service.js
@@ -1,14 +1,6 @@
 angular.module('questCreator').service('GameService', function () {
 
-    var gameDetail = {
-      // thumbnail: "http://cdn.akamai.steamstatic.com/steam/apps/345390/extras/KQ_CC-PC_Bundle-Art_Capsule_Main.png?t=1477527248",
-      // name: "King's Quest Collection",
-      // creator: "billy badass",
-      // players: 6,
-      // created_at: new Date(),
-      // responseText: "something",
-      // totalPoints: 75
-    };
+    var gameDetail = {};
 
     function loadGame(name) {
       $.ajax({
