5e1d3f03b58473a2ebc7d6baacb9487da40fbed8
Andrew Toups <mistertoups@gmail.com>
2016-11-19 16:59:25 -0600

    Finished implementations of popup directive

--- public/src/js/directives/popup.directive.js
+++ public/src/js/directives/popup.directive.js
@@ -1,5 +1,37 @@
-angular.module('questCreator').directive('popup', function(){
+angular.module('questCreator')
+.directive('popup', function(){
   return {
+    scope: {
+      title: '=popupTitle'
+    },
+    replace: true,
+    transclude: true,
+    link: function (scope, element, attrs) {
+      scope.kill = function(){
+        $('#overlay').remove();
+      }
+    },
     templateUrl: './src/views/popup.html'
   };
+});
+
+
+
+angular.module('questCreator')
+.directive('popupContent', function(){
+
+  var popups = {
+    'editor': 'editor.html',
+  }
+
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    templateUrl: function(){
+    return './src/views/popups/' + popups[0]
+
+    }
+
+  }
 });
