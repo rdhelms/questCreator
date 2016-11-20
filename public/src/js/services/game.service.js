2c6b7c6c562b6004c949994700ca5f67e2cac52a
Jason Carr <jasonhcarr@gmail.com>
2016-11-20 16:33:11 -0500

    work on game service

--- public/src/js/services/game.service.js
+++ public/src/js/services/game.service.js
@@ -1,34 +1,49 @@
-angular.module('questCreator').service('GameService', function () {
+angular.module('questCreator').service('GameService', function() {
 
     var gameDetail = {};
 
     function loadGame(name) {
-      $.ajax({
-        method: 'GET',
-        url: 'https://forge-api.herokuapp.com/games/load',
-        data: JSON.stringify(name),
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function(response) {
-          return response;
-        },
-        error: function(error) {
-          alert('There was a problem loading this game');
-        }
-      });
-}
+        $.ajax({
+            method: 'GET',
+            url: 'https://forge-api.herokuapp.com/games/load',
+            data: JSON.stringify(name),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(response) {
+                return response;
+            },
+            error: function(error) {
+                alert('There was a problem loading this game');
+            }
+        });
+    }
 
-function getGameDetail() {
-  return gameDetail;
-}
+    function getAllGames() {
+        $.ajax({
+            method: 'GET',
+            url: 'https://forge-api.herokuapp.com/games/all',
+            contentType: 'application/json',
+            success: function(response) {
+                return response;
+            },
+            error: function(error) {
+                alert('There was a problem loading the games');
+            }
+        });
+    }
 
-function setGameDetail(game) {
-  gameDetail = game;
-}
+    function getGameDetail() {
+        return gameDetail;
+    }
 
-return {
-  loadGame: loadGame,
-  getGameDetail: getGameDetail,
-  setGameDetail: setGameDetail
-};
+    function setGameDetail(game) {
+        gameDetail = game;
+    }
+
+    return {
+        loadGame: loadGame,
+        getGameDetail: getGameDetail,
+        setGameDetail: setGameDetail,
+        getGames: getAllGames
+    };
 });
