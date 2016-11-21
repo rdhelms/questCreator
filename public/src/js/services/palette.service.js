e84a8bc18a46703eadfa03cbb25ea5b62fa97a38
Jason Carr <jasonhcarr@gmail.com>
2016-11-20 19:18:55 -0500

    basic functionality for the palette service; untested

--- public/src/js/services/palette.service.js
+++ public/src/js/services/palette.service.js
@@ -1,11 +1,28 @@
-angular.module('questCreator').service('PaletteService', function () {
-  function getAllAssets() {
+angular.module('questCreator').service('PaletteService', function (UserService) {
+
+  var currentType = '';
+
+  var assets = null;
+
+  var headerData = {
+    user_id: UserService.get().id,
+    token: UserService.get().token
+  };
+
+  function getAssetsInService() {
+    return assets;
+  }
+
+  function getCurrentType() {
+    return currentType;
+  }
+
+  function getAllAssets(UserService) {
       $.ajax({
           method: 'GET',
           url: 'https://forge-api.herokuapp.com/articles/index',
           contentType: 'application/json',
           success: function(response) {
-              console.log('here');
               return response;
           },
           error: function(error) {
@@ -14,7 +31,53 @@ angular.module('questCreator').service('PaletteService', function () {
       });
   }
 
+  function getAssetsByType(type) {
+    currentType = type;
+    var url = '';
+    if (type === 'bg') {
+      url = 'backgrounds/all';
+    } else if (type === 'obj') {
+      url = 'obstacles/all';
+    } else if (type === 'ent'){
+      url = 'entities/all';
+    }
+    return $.ajax({
+      method: 'GET',
+      url: 'https://forge-api.herokuapp.com/' + url,
+      headers: headerData,
+      success: function(response) {
+        console.log(response);
+        assets = response;
+        return assets;
+      },
+      error: function(error) {
+        alert('There was a problem loading the ' + currentType +'s');
+      }
+    });
+  }
+
+  function getAssetsByTag(tag) {
+    return $.ajax({
+      method: 'GET',
+      url: 'https://forge-api.herokuapp.com/' + currentType + '/search',
+      headers: headerData,
+      data: tag,
+      success: function(response) {
+        console.log(response);
+        assets = response;
+        return assets;
+      },
+      error: function(error) {
+        alert('There was a problem loading the ' + currentType +'s matching your search for ' + tag);
+      }
+    });
+  }
+
   return {
-    getAssets: getAllAssets
+    getCurrent: getAssetsInService,
+    getCurrentType: getCurrentType,
+    getAll: getAllAssets,
+    getByType: getAssetsByType,
+    getByTag: getAssetsByTag
   };
 });
