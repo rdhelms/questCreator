2e64bcbe92bd0ccc90b886aa2f2ecddf68075d6e
Jason Carr <jasonhcarr@gmail.com>
2016-11-22 14:25:16 -0500

    fixed issues caused by merge conflict resolution

--- public/src/js/controllers/profile.controller.js
+++ public/src/js/controllers/profile.controller.js
@@ -2,56 +2,52 @@ angular.module('questCreator').controller('profileCtrl', function(socket, $state
 
     UserService.checkLogin().then(function(response) {
 
-      $scope.user = null;
-      $scope.games = null;
+        $scope.games = null;
 
-      UserService.get().then(function(user) {
-        $scope.user = user;
-        $scope.$apply();
-      });
+        $scope.user = UserService.get();
 
-      $scope.getJoinedDate = function(date) {
-          return new Date(date);
-      };
+        $scope.getJoinedDate = function(date) {
+            return new Date(date);
+        };
 
-      UserService.getUserGames().done(function(games) {
-          $scope.games = games;
-          $scope.$apply();
-      });
+        UserService.getUserGames().done(function(games) {
+            $scope.games = games;
+            $scope.$apply();
+        });
 
-      // $scope.collaborators = UserService.getCollabRequests().done(function (collaborators) {
-      //   $scope.$apply();
-      // });
+        // $scope.collaborators = UserService.getCollabRequests().done(function (collaborators) {
+        //   $scope.$apply();
+        // });
 
-      $scope.createGame = function() {
-          $scope.user.editGame = null;
-          UserService.set($scope.user);
-          $state.go('main.game.editor.views');
-      };
+        $scope.createGame = function() {
+            $scope.user.editGame = null;
+            UserService.set($scope.user);
+            $state.go('main.game.editor.views');
+        };
 
-      $scope.editGame = function(name) {
-          $scope.user.editGame = name;
-          UserService.set($scope.user);
-          $state.go('main.game.editor.views');
-      };
+        $scope.editGame = function(name) {
+            $scope.user.editGame = name;
+            UserService.set($scope.user);
+            $state.go('main.game.editor.views');
+        };
 
-      $scope.archiveGame = function(game) {
-          var agree = confirm("Are you sure you wanna archive '" + game.name + "'? That means no one will be able to play it and all player information will be lost. You will NOT be able to retrieve this later");
-          if (agree) {
-              UserService.archive(game.id);
-          }
-      };
+        $scope.archiveGame = function(game) {
+            var agree = confirm("Are you sure you wanna archive '" + game.name + "'? That means no one will be able to play it and all player information will be lost. You will NOT be able to retrieve this later");
+            if (agree) {
+                UserService.archive(game.id);
+            }
+        };
 
-      $scope.showCollaborators = function () {
+        $scope.showCollaborators = function() {
 
-      };
+        };
 
-      $scope.toggleCollab = function (collab) {
-        UserService.toggleAccepted(collab.game);
-      };
+        $scope.toggleCollab = function(collab) {
+            UserService.toggleAccepted(collab.game);
+        };
 
-      $scope.removeRequest = function (collab) {
-        UserService.toggleRequested(collab.game);
-      };
+        $scope.removeRequest = function(collab) {
+            UserService.toggleRequested(collab.game);
+        };
     });
 });
