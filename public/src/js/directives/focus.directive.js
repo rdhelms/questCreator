060f2c764fda52d5ed4d2a0cb913f7d0eeba5c9a
Andrew Toups <mistertoups@gmail.com>
2016-11-27 14:24:49 -0500

    Added auto focus directive

---
+++ public/src/js/directives/focus.directive.js
@@ -0,0 +1,17 @@
+angular.module('questCreator')
+.directive('focus', ['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+        //scope: true,   // optionally create a child scope
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.focus);
+            scope.$watch(model, function (value) {
+                console.log('value=', value);
+                if (value === true) {
+                    $timeout(function () {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+    };
+}]);
