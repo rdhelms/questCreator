3e7192821700e199a7f3f0da583af1a87e177f4a
Jason Carr <jasonhcarr@gmail.com>
2016-11-28 23:46:28 -0500

    added local storage service

---
+++ public/src/js/services/localStorage.service.js
@@ -0,0 +1,14 @@
+angular.module('questCreator').service('StorageService', function (localStorageService) {
+  function getFromStorage(key) {
+
+  }
+
+  function setInStorage(key, value) {
+
+  }
+
+  return {
+    get: getFromStorage,
+    set: setInStorage
+  };
+});
