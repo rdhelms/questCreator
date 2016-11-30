d8e1e8d874762da49b9f5395fe46e02d2416b1fe
Robby Helms <robert.helms1@gmail.com>
2016-11-30 15:34:45 -0500

    more socket removal

--- public/lib/js/app.js
+++ public/lib/js/app.js
@@ -1369,19 +1369,19 @@ angular.module('questCreator')
   };
 });
 ;angular.module('questCreator').service('socket', function() {
-  var socket = io();
-
-  return {
-    on: function(eventName, callback){
-      socket.on(eventName, callback);
-    },
-    emit: function(eventName, data) {
-      socket.emit(eventName, data);
-    },
-    off: function(eventName, data) {
-      socket.off(eventName, data);
-    }
-  };
+  // var socket = io();
+  //
+  // return {
+  //   on: function(eventName, callback){
+  //     socket.on(eventName, callback);
+  //   },
+  //   emit: function(eventName, data) {
+  //     socket.emit(eventName, data);
+  //   },
+  //   off: function(eventName, data) {
+  //     socket.off(eventName, data);
+  //   }
+  // };
 });
 ;angular.module('questCreator')
     .service('UserService', function(PopupService, $rootScope) {
